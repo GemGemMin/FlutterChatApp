70cffe9673aab058ac06d794ef38755b858927d1
GemGemMin <kkm32543@khu.ac.kr>
2024-08-24 15:14:45 +0900

    change chatScreen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,7 +2,6 @@ import 'package:chatapp/chatting/chat/message.dart';
 import 'package:chatapp/chatting/chat/new_message.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
